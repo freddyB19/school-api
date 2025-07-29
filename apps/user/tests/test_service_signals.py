de2e2451268cb6254f018272d14fed438603f557
freddyB19 <freddyybolivar2013@gmail.com>
2025-07-28 22:50:30 -0400

    test: agregando tests para el servicio del signal change_role

--- apps/user/tests/test_service_signals.py
+++ apps/user/tests/test_service_signals.py
@@ -1,16 +1,31 @@
 from unittest.mock import (patch, Mock)
 
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+
 from django.test import TestCase
 
 from apps.user.services.signals.signals import (
 	reset_password,
+	change_role,
+	SignalChangeRole,
 	SignalResetPassword,
-
 )
 
+from apps.user.models import TypeRole, User
 from apps.user.services.signals.receivers import (
+	change_role_receiver,
 	reset_password_receiver
 )
+
+from .utils.utils import create_user
+
+def create_permissions():
+	content_type = ContentType.objects.get_for_model(User)
+	permission = Permission.objects.create(
+		codename="can_change_user_role",
+		content_type=content_type,
+	)
 
 
 class UserServiceResetPassword(TestCase):
@@ -68,3 +83,92 @@ class UserServiceResetPassword(TestCase):
 			reset_password.connect(reset_password_receiver, sender = SignalResetPassword)
 
 
+class UserServiceChangeRole(TestCase):
+
+	def test_signal_change_role_emits(self):
+		"""
+			Validar que el signal 'change_role' es emitido
+		"""
+
+		with patch.object(change_role, 'send') as mock_send:
+			signal = SignalChangeRole()
+			
+			role = TypeRole.admin
+			user = create_user()
+
+			signal.send(role = role, user = user)
+
+			mock_send.assert_called_once()
+			mock_send.assert_called_once_with(
+				sender = SignalChangeRole,
+				role = role,
+				user = user,
+			)
+
+	def test_signal_change_role_with_wrong_params(self):
+		"""
+			Enviando parametros incorrectos al método send del signal
+		"""
+		test_cases = [
+			{
+				"values": {
+					"role": TypeRole.admin,
+					"user": 1
+				},
+				"expect": {
+					"raise": ValueError,
+					"message": "Debe enviar un usuario, no un [<class 'int'>] - 1" 
+				}
+			},
+			{
+				"values": {
+					"role": 12,
+					"user": create_user()
+				},
+				"expect": {
+					"raise": ValueError,
+					"message": f"Debe ser un role valido {TypeRole.choices}"
+				}
+			}
+		]
+
+		for case in test_cases:
+			with self.subTest(case = case):
+				signal = SignalChangeRole()
+
+				with self.assertRaisesMessage(case['expect']['raise'], case['expect']['message'] ):
+					signal.send(**case['values'])
+
+
+
+
+	def test_signal_change_role_with_mock_receiver(self):
+		"""
+			Mockeando el receiver, validar que el receptor de 'change_role' se ejecuta correctamente
+		"""
+		create_permissions()
+
+		mock_receiver = Mock()
+
+		change_role.disconnect(change_role_receiver, sender = SignalChangeRole)
+
+		change_role.connect(mock_receiver, sender = SignalChangeRole)
+
+		role = TypeRole.admin
+		user = create_user(role = 0)
+
+		try:
+			signal = SignalChangeRole()			
+
+			signal.send(role = role, user = user)
+
+			mock_receiver.assert_called_once()
+			mock_receiver.assert_called_once_with(
+				sender = SignalChangeRole,
+				role = role,
+				user = user,
+				signal = change_role
+			)
+			
+		finally:
+			reset_password.connect(change_role_receiver, sender = SignalChangeRole)
