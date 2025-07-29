76d61b7ddcee2b0966ed15197a7e98ec167e6719
freddyB19 <freddyybolivar2013@gmail.com>
2025-07-28 22:39:22 -0400

    refactor: moviendo clases usados por los receivers a signals/utils

--- apps/user/services/signals/receivers.py
+++ apps/user/services/signals/receivers.py
@@ -1,10 +1,6 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth.models import Permission
 
 from apps.emails.services.reset_password import EmailResetPassword
-from apps.user.model import User, TypeRole
 
 from .signals import (
 	SignalResetPassword, 
@@ -12,11 +8,8 @@ from .signals import (
 	SignalChangeRole,
 	change_role
 )
+from .utils.class_receivers import ChangeRole
 
-
-subject = "Restableciendo contraseña"
-from_email = "freddy@gmail.com"
-to = []
 
 @receiver(reset_password, sender=SignalResetPassword)
 def reset_password_receiver(sender, **kwargs):
@@ -33,60 +26,14 @@ def reset_password_receiver(sender, **kwargs):
 		email.send()
 
 
-class ChangePermissionAdmin:
-	def __init__(self, user: User) -> None:
-		self.user = user
-
-		if user.role != TypeRole.admin:
-			raise ValueError(f"El usuario debe tener role 'admin' - [id, role]: [{user.id}, {user.role}]")
-
-	def change_permissions(self):
-		if not user.has_perm("user.can_change_user_role"):
-			content_type = ContentType.objects.get_for_model(User)
-			permission = Permission.objects.get(
-				codename="can_change_user_role",
-				content_type=content_type,
-			)
-			user.user_permissions.add(permission)
-
-
-class ChangePermissionStaff:
-	def __init__(self, user: User) -> None:
-		self.user = user
-
-		if user.role != TypeRole.staff:
-			raise ValueError(f"El usuario debe tener role 'staff' - [id, role]: [{user.id}, {user.role}]")
-
-	def change_permissions(self):
-		if user.has_perm("user.can_change_user_role"):
-			content_type = ContentType.objects.get_for_model(User)
-			permission = Permission.objects.get(
-				codename="can_change_user_role",
-				content_type=content_type,
-			)
-			user.user_permissions.remove(permission)
-
-
-class ChangeRole:
-	def get(cls, type: int, user: User):
-
-		if type == TypeRole.admin:
-			# add permiso
-			return ChangePermissionAdmin(user = user)
-		elif type == TypeRole.staff:
-			# validar que lo tiene
-			# remover permiso
-			return ChangePermissionStaff(user = user)
-		else:
-			# None
-			return None
-
-
 @receiver(change_role, sender = SignalChangeRole)
-def change_role_to_admin(sender, **kwargs):
+def change_role_receiver(sender, **kwargs):
 	if "role" in kwargs and "user" in kwargs:
 		
-		role = ChangeRole(type = kwargs["role"], user = kwargs["user"])
+		role = ChangeRole.get(
+			type = kwargs.get("role"), 
+			user = kwargs.get("user")
+		)
 
 		if role:
 			role.change_permissions()
