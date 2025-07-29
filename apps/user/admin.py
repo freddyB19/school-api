4a36ad523816c62f98966a3b77d3bffe2ae98ce7
freddyB19 <freddyybolivar2013@gmail.com>
2025-07-28 20:38:39 -0400

    feat: cambiando configuración del panel de 'admin' de la tabla usuario

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,19 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from django.contrib.auth.models import Permission
 from . import models
 
 # Register your models here.
-class AdminUser(admin.ModelAdmin):
+class AdminUser(UserAdmin):
+	ordering = ["id"]
+
 	list_display = [
 		"id",
 		"name",
 		"email",
-		"role"
+		"role",
 	]
 
 	list_filter = [
 		"role"
 	]
 
+	fieldsets = [
+		(
+			None,
+			{"fields": ("email", "password", "role", "user_permissions")}
+		),
+		(
+			"Permissions",
+			{"fields": ("is_admin" ,"is_staff", "is_active", "is_superadmin", "is_superuser")}
+		)
+	]
+	add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("name", "role", "user_permissions", "email", "password1", "password2")
+        }),
+    )
+
 
 
 admin.site.register(models.User, AdminUser)
+admin.site.register(Permission)
