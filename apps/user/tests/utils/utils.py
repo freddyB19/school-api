f77e9899fd866aaa2309d6c8950f57bbed44c430
freddyB19 <freddyybolivar2013@gmail.com>
2025-08-13 01:10:05 -0400

    feat: agregando función 'create_permissions' para ser usando en los tests

--- apps/user/tests/utils/utils.py
+++ apps/user/tests/utils/utils.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+
 from faker import Faker
 
 from apps.user import models
-from rest_framework.test import APIClient
+
 
 
 faker = Faker(locale="es")
@@ -21,11 +24,19 @@ class FakerCreateUser:
 		self.role = ROLE
 		self.password = PASSWORD
 
-
 def create_user(name:str = "Jose", email:str = "jose@gmail.com", password:str = "12345", role:int = 1) -> models.User:
 	return models.User.objects.create_user(
 		name = name,
 		email = email,
 		password = password,
 		role = role
+	)
+
+
+def create_permissions(name = "Test permission", codename = "test_permission"):
+	content_type = ContentType.objects.get_for_model(models.User)
+	permission = Permission.objects.create(
+		name = name,
+		codename= codename,
+		content_type=content_type,
 	)
