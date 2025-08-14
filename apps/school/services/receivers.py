5d3384dcbf4d46d8577012eabf3b5561aec5e31b
freddyB19 <freddyybolivar2013@gmail.com>
2025-08-14 17:47:09 -0400

    feat: nuevo signal para generar una tabla 'setting_format' al crear una escuela

--- apps/school/services/receivers.py
+++ apps/school/services/receivers.py
@@ -1,10 +1,16 @@
 from django.dispatch import receiver
 from django.db.models import signals
 
-from apps.school.models import School
+from apps.school.models import School, SettingFormat
 from apps.management.models import Administrator 
 
 @receiver(signals.post_save, sender=School)
 def create_administrator_site(sender, instance=None, created=False, **kwargs):
 	if created:
 		Administrator.objects.create(school = instance)
+
+
+@receiver(signals.post_save, sender=School)
+def create_setting_format(sender, instance=None, created=False, **kwargs):
+	if created:
+		SettingFormat.objects.create(school = instance)
