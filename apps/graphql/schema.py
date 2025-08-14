5f172604766708d9a6cc2e720dc44bd84e35f8c6
freddyB19 <freddyybolivar2013@gmail.com>
2025-08-14 00:44:28 -0400

    feat: integrando 'querys' de graphql/management al esquema principal

--- apps/graphql/schema.py
+++ apps/graphql/schema.py
@@ -2,9 +2,11 @@ import graphene
 
 from .school.querys import SchoolQuery
 from .management_user.mutations import ManagementUserMutation
+from .management.querys import AdministratorDetailQuery
 
 class Query(
 	SchoolQuery,
+	AdministratorDetailQuery,
 	graphene.ObjectType
 ):
 	pass
