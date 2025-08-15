d441f1a08a5bd11c73eee74f5facb1e2d95c3de9
freddyB19 <freddyybolivar2013@gmail.com>
2025-08-15 18:17:35 -0400

    test: esquema de las consultas para los tests de graphql/school

---
+++ apps/graphql/tests/school/utils/schemas.py
@@ -0,0 +1,79 @@
+QUERY_SCHOOL_BY_SUBDOMAIN = """
+query School($subdomain: String!) {
+	schoolBySubdomain(subdomain: $subdomain) {
+		school {
+			id
+			name
+			subdomain
+		}
+		
+		settings {
+			color
+		}
+
+		networks {
+			profile
+		}
+		
+		news {
+			id
+			title
+		}
+
+		coordinates {
+			id
+			title
+		}
+
+	}
+}
+"""
+
+
+QUERY_SCHOOL_SERVICE = """
+query SchoolService($schoolId: Int!){
+	schoolServiceOnline(schoolId: $schoolId) {
+		downloads {
+			id
+			title
+		}
+		
+		repositories {
+			id
+			project
+		}
+	}
+
+	schoolServiceOffline(schoolId: $schoolId) {
+		infraestructure {
+			id
+			photo
+		}
+	}
+}
+
+"""
+
+
+QUERY_SCHOOL_CALENDAR = """
+query SchoolCalendar($subdomain: String!, $month: Months, $first: Int) {
+	schoolCalendar(subdomain: $subdomain, month: $month, first: $first) {
+		pageInfo {
+	        startCursor
+	        endCursor
+	        hasNextPage
+	        hasPreviousPage
+	    }
+	    edges {
+	        cursor
+	        node {
+				id
+				title
+				date
+				calendarId
+	        }
+        }
+
+	}
+}
+"""
