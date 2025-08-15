970cf8e000fa28138d53dee82a5206362b22a388
freddyB19 <freddyybolivar2013@gmail.com>
2025-08-15 17:22:59 -0400

    feat: agregando un nuevo resolver para la paginación de 'schoolCalendar'

--- apps/graphql/school/querys.py
+++ apps/graphql/school/querys.py
@@ -1,13 +1,15 @@
 from django.utils import timezone
 
 import graphene
+from graphene import relay
 
 from apps.school import models
 
 from .types import (
+	Months,
 	SchoolType,
 	NewsType,
-	CalendarType,
+	CalendarConnection,
 	SettingsType,
 	SocialMediaType,
 	CoordinateType,
@@ -39,17 +41,22 @@ class SchoolQuery(graphene.ObjectType):
 		schoolId = graphene.Int(required = True)
 	)
 
+	schoolCalendar = relay.ConnectionField(
+		CalendarConnection,
+		subdomain = graphene.String(required = True),
+		month = graphene.Argument(Months),
+	)
+
 
 	def resolve_schoolBySubdomain(root, info, subdomain):
 		try:
-			school = models.School.objects..get(subdomain = subdomain)
+			school = models.School.objects.get(subdomain = subdomain)
 		except models.School.DoesNotExist as e:
 			return SchoolDoesNotExist
 
 		current_month = timezone.now().month
 
 		settings = school.setting
-		calendar = school.calendarsList.filter(date__month = current_month)
 		news = school.newsList.filter(status = "publicado")[:10]		
 		socialMedia = school.socialMediasList.all()[:6]
 		coordinates = school.coordinatesList.all()
@@ -57,13 +64,27 @@ class SchoolQuery(graphene.ObjectType):
 		return SchoolHomePageType(
 			school = school,
 			news = news,
-			calendar = calendar,
 			settings = settings,
 			networks = socialMedia,
 			coordinates = coordinates
 		)
 
+
+	def resolve_schoolCalendar(root, info, subdomain, month = None, **kwargs):
+		try:
+			school = models.School.objects.get(subdomain = subdomain)
+		except models.School.DoesNotExist as e:
+			return models.School.objects.none()
+
+		current_month = timezone.now().month if not month else Months.get(month).value
+
+		calendar = school.calendarsList.filter(
+			date__month = current_month
+		)
+
+		return calendar
 	
+
 	def resolve_schoolServiceOffline(root, info, schoolId):
 		infraestructure = models.Infraestructure.objects.filter(
 			school_id = schoolId
@@ -72,6 +93,7 @@ class SchoolQuery(graphene.ObjectType):
 		return ServiceOfflineType(
 			infraestructure = infraestructure
 		)
+
 
 	def resolve_schoolServiceOnline(root, info, schoolId):
 		downloads = models.Download.objects.filter(
