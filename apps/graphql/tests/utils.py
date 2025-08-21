8e664029030199b32a1b73ebc352a9b97907d041
freddyB19 <freddyybolivar2013@gmail.com>
2025-08-21 18:20:14 -0400

    test-feat: agregando función 'get_token' a 'graphql/test-utils'

--- apps/graphql/tests/utils.py
+++ apps/graphql/tests/utils.py
@@ -1,8 +1,11 @@
 import datetime
 from typing import TypeVar
 
+from django.urls import reverse
+from django.test import Client
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from apps.user.tests.utils.utils import FakerCreateUser
 
 import jwt
 
@@ -28,3 +31,14 @@ def encode_token(user: User, exp_time:dict = None) -> str:
 		SECRET_KEY, 
 		algorithm = ALGOTITHM
 	)
+
+
+def get_token(email: str = "auth@example.com", password: str = "1234") -> str:
+	client = Client()
+
+	response = client.post(reverse("user:obtain_token_pair"), {
+		"email": email,
+		"password": password
+	})
+
+	return response.data["access"]
