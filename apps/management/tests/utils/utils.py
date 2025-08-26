f745f3ed59d15167a8588a6937ca06553215614e
freddyB19 <freddyybolivar2013@gmail.com>
2025-08-26 16:06:03 -0400

    test: agregando función 'get_long_string' a los 'tests/utils' de 'management'

--- apps/management/tests/utils/utils.py
+++ apps/management/tests/utils/utils.py
@@ -1,5 +1,9 @@
 from apps.management import models
 
+from faker import Faker
+
+faker = Faker(locale = "es")
+
 def get_administrator(school_id: int = None):
 	if not school_id:
 		raise ValueError("Debe ser el ID de 'school'")
@@ -8,3 +12,7 @@ def get_administrator(school_id: int = None):
 		return models.Administrator.objects.get(school_id = school_id)
 	except models.Administrator.DoesNotExist as e:
 		return None
+
+
+def get_long_string():
+	return f"{faker.paragraph(nb_sentences = 5)}{faker.paragraph(nb_sentences = 5)}"
