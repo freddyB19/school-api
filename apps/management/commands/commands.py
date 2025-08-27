c8f08a3f6d0ce05065a2371ee18957fc3c375bd6
freddyB19 <freddyybolivar2013@gmail.com>
2025-08-26 21:54:32 -0400

    style: integrando validaciones de parametros con 'pydantic' en 'update_school_logo'

--- apps/management/commands/commands.py
+++ apps/management/commands/commands.py
@@ -1,5 +1,8 @@
+from typing import TypeVar
 
-from pydantic import validate_call
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+from pydantic import validate_call, ConfigDict
 
 from faker import Faker
 
@@ -7,11 +10,18 @@ from apps.utils.result_commands import ResultCommand
 
 faker = Faker(locale="es")
 
-def update_school_logo(image = None) -> ResultCommand:
+UploadedFile = TypeVar("UploadedFile", bound=InMemoryUploadedFile)
+
+
+@validate_call(config = ConfigDict(hide_input_in_errors=True, arbitrary_types_allowed = True))
+def update_school_logo(image:InMemoryUploadedFile = None) -> ResultCommand:
 	context = {
 		"status": False,
 	}
 
+	if not image:
+		context.update({"errors": ["Debe pasar una imagen"]})
+	
 	# Conectarme a un servicio para subir la imagen
 
 	context.update({"query": faker.image_url(), "status": True})
