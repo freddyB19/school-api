3eba9331f67c4e1c18ba87492ac8df6712bb8ccb
freddyB19 <freddyybolivar2013@gmail.com>
2025-08-26 22:30:51 -0400

    test: agregando tests para validar la autenticación de usuario en 'AdministratorAPITest'

--- apps/management/tests/test_api.py
+++ apps/management/tests/test_api.py
@@ -60,7 +60,7 @@ class AdministratorAPITest(TransactionTestCase):
 		self.assertEqual(responseJson["total_users"], self.administrator.users.count())
 
 
-	def test_get_administrator_with_no_existent_school(self):
+	def test_get_administrator_does_not_existent_school(self):
 		"""
 			Intentar acceder a la información de 'administrator' con un ID de escuela que no existe
 		"""
@@ -78,6 +78,17 @@ class AdministratorAPITest(TransactionTestCase):
 
 		self.assertEqual(responseStatus, 404)
 		self.assertEqual(responseJson["error"]["message"], f"No existe información sobre los administradores del portal de un colegio con ID: {wrong_school_id}")
+
+	def test_get_administrator_without_authentication(self):
+		"""
+			Accediendo a la información de 'administrator' sin autenticación del usuario
+		"""
+		response = self.client.get(self.URL_ADMINISTRATOR)
+
+		responseJson = response.data
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 401)
 
 
 	def test_get_detail_administrator(self):
@@ -103,7 +114,7 @@ class AdministratorAPITest(TransactionTestCase):
 			Intentar acceder a una información que no existe en endpoint administrator-detail
 		"""
 		self.client.force_authenticate(user = self.user_role_staff)
-		
+
 		wrong_id = 120
 		
 		response = self.client.get(reverse(
@@ -117,6 +128,17 @@ class AdministratorAPITest(TransactionTestCase):
 		self.assertEqual(responseStatus, 404)
 		self.assertEqual(responseJson["error"]["message"], f"No existe información sobre los administradores de este portal con ID: {wrong_id}")
 	
+
+	def test_get_detail_administrator_without_authentication(self):
+		"""
+			Accediendo al endpoint 'administrator-detail' sin autenticación
+		"""
+		response = self.client.get(self.URL_ADMINISTRATOR_DETAIL)
+
+		responseJson = response.data
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 401)
 
 
 class UserUpdatePermissionsTest(TransactionTestCase):
