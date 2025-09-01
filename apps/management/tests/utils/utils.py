adc3c6fd83684c57916e019e93b8f002e17ea577
freddyB19 <freddyybolivar2013@gmail.com>
2025-09-01 18:06:40 -0400

    test-feat: agregando función 'create_list_images' para los 'tests/management'

--- apps/management/tests/utils/utils.py
+++ apps/management/tests/utils/utils.py
@@ -1,5 +1,8 @@
+import tempfile
+
 from apps.management import models
 
+from PIL import Image
 from faker import Faker
 
 faker = Faker(locale = "es")
@@ -16,3 +19,18 @@ def get_administrator(school_id: int = None):
 
 def get_long_string():
 	return f"{faker.paragraph(nb_sentences = 5)}{faker.paragraph(nb_sentences = 5)}"
+
+
+def create_list_images() -> list:
+	images = []
+		
+	for _ in range(3):
+		temp_file = tempfile.NamedTemporaryFile(suffix = ".jpg")
+
+		image = Image.new("RGB", (10, 10))
+		image.save(temp_file, format="JPEG")
+		temp_file.seek(0)
+		
+		images.append(temp_file)
+
+	return images
