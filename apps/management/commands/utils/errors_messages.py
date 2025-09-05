df08a4b9aab4213baa200c6b3594b19f21049f66
freddyB19 <freddyybolivar2013@gmail.com>
2025-09-04 22:30:41 -0400

    feat: agregando archivo 'errors_messages' en los 'commands/utils' de management

---
+++ apps/management/commands/utils/errors_messages.py
@@ -0,0 +1,9 @@
+
+class NewsParamErrorsMessages:
+	MAX_LEN = "El título de la noticia es muy largo"
+	MIN_LEN = "El título de la noticia es muy corto"
+	INVALID_STATUS = "Opción incorrecta para el estado de la noticia"
+
+
+class SchoolErrorsMessages:
+	DoesNotExist = "No existe información sobre esta escuela"
