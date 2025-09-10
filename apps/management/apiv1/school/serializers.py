e9712a1ccc1a368560a3d179887f2a66c366e071
freddyB19 <freddyybolivar2013@gmail.com>
2025-09-09 22:21:59 -0400

    feat: agregando serializadores [NewsRequest, NewsListResponse] en 'management/apiv1/school'
    Se agregaron funciones anónimas y variables constantes, para la
    validación de algunos campos de los serializadores.

--- apps/management/apiv1/school/serializers.py
+++ apps/management/apiv1/school/serializers.py
@@ -2,14 +2,11 @@ from apps.school import models
 
 from rest_framework import serializers
 
+from apps.school import models as school_models
+from apps.school.apiv1 import serializers as school_serializer
+
+
 ERROR_FIELD = lambda field, type, simbol, value: f"El campo '{field}' es muy {type}, debe ser {simbol} a {value}" 
-
-MAX_LENGTH_ADDRESS = 100
-MIN_LENGTH_ADDRESS = 10
-
-MAX_LENGTH_NAME = 50
-MIN_LENGTH_NAME = 5
-
 
 MIN_LENGTH_FIELDS = 10
 
@@ -30,38 +27,38 @@ class SchoolUpdateRequest(serializers.ModelSerializer):
 
 		extra_kwargs = {
 			"name": {
-				"max_length": MAX_LENGTH_NAME,
-				"min_length": MIN_LENGTH_NAME,
+				"max_length": models.MAX_LENGTH_SCHOOL_ADDRESS,
+				"min_length": models.MIN_LENGTH_SCHOOL_ADDRESS,
 				"error_messages": {
 					"max_length": ERROR_FIELD(
 						field = "nombre", 
 						type = "largo",
 						simbol = "menor o igual",
-						value = MAX_LENGTH_NAME
+						value = models.MAX_LENGTH_SCHOOL_NAME
 					),
 					"min_length": ERROR_FIELD(
 						field = "nombre", 
 						type = "corto",
 						simbol = "mayor o igual",
-						value = MIN_LENGTH_NAME
+						value = models.MIN_LENGTH_SCHOOL_NAME
 					)
 				}
 			},
 			"address": {
-				"max_length": MAX_LENGTH_ADDRESS,
-				"min_length": MIN_LENGTH_ADDRESS,
+				"max_length": models.MAX_LENGTH_SCHOOL_ADDRESS,
+				"min_length": models.MIN_LENGTH_SCHOOL_ADDRESS,
 				"error_messages": {
 					"max_length": ERROR_FIELD(
 						field = "dirección", 
 						type = "largo",
 						simbol = "menor o igual",
-						value = MAX_LENGTH_ADDRESS
+						value = models.MAX_LENGTH_SCHOOL_ADDRESS
 					),
 					"min_length": ERROR_FIELD(
 						field = "misión",
 						type = "corto",
 						simbol = "mayor o igual",
-						value = MIN_LENGTH_ADDRESS
+						value = models.MIN_LENGTH_SCHOOL_ADDRESS
 					)
 				}
 			},
@@ -103,4 +100,58 @@ class SchoolUpdateRequest(serializers.ModelSerializer):
 
 class SchoolUpdateLogoRequest(serializers.Serializer):
 	logo = serializers.ImageField(max_length = 20)
+
+
+
+MAX_LENGTH_IMAGE_NAME = 20
+
+STATUS_INVALID_CHOICE = "La opción elegida es invalida"
+
+class NewsRequest(serializers.ModelSerializer):
+	media = serializers.ListField(
+		required = False,
+		child = serializers.ImageField(max_length = MAX_LENGTH_IMAGE_NAME), 
+	)
 	
+	class Meta:
+		model = models.News
+		fields = [
+			"title",
+			"media",
+			"status",
+			"description",
+		]
+
+		extra_kwargs = {
+			"title": {
+				"max_length": models.MAX_LENGTH_NEWS_TITLE,
+				"min_length": models.MIN_LENGTH_NEWS_TITLE,
+				"error_messages": {
+					"max_length": ERROR_FIELD(
+						field = "titulo", 
+						type = "largo",
+						simbol = "menor o igual",
+						value = models.MAX_LENGTH_NEWS_TITLE
+					),
+					"min_length": ERROR_FIELD(
+						field = "titulo", 
+						type = "corto",
+						simbol = "mayor o igual",
+						value = models.MIN_LENGTH_NEWS_TITLE
+					)
+				}
+
+			},
+			"status": {
+				"error_messages": {
+					"invalid_choice": STATUS_INVALID_CHOICE
+				}
+			}
+		}
+
+
+class NewsListResponse(school_serializer.NewsListResponse):
+
+	class Meta:
+		model = school_models.News
+		fields = ["id", "title", "created", "updated", "media", "status"]
