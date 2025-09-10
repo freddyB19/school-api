9c6ceea4c139546c1da4a069478bf5ca4ec63d52
freddyB19 <freddyybolivar2013@gmail.com>
2025-09-09 22:27:10 -0400

    feat: agregando nuevo permiso 'BelongToOurAdministrator' a 'management/apiv1/school'

--- apps/management/apiv1/school/permissions.py
+++ apps/management/apiv1/school/permissions.py
@@ -1,5 +1,26 @@
 from rest_framework import permissions
 
+from apps.management import models
 
 class IsUserPermission(permissions.DjangoModelPermissions):
 	message = "Este usuario no posee los permisos necesarios para realizar esta operación" 
+
+
+
+class BelongToOurAdministrator(permissions.BasePermission):
+	"""
+		Este permiso sirve para limitar que solo se pueda acceder/modificar
+		la información del colegio al que formamos parte como administradores.
+	"""
+	message = "No tienes permisos de modificar información que no te pertenece"
+	
+	def has_permission(self, request, view):
+
+		school_id = view.kwargs.get("school_id")
+
+		admin_school = models.Administrator.objects.filter(
+			school_id = school_id,
+			users__id = request.user.id
+		).exists()
+
+		return admin_school
