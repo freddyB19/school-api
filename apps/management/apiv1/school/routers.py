291eac65b9c47c1d044e0b66e6f9c1c3c482cdc3
freddyB19 <freddyybolivar2013@gmail.com>
2025-09-14 20:50:45 -0400

    feat: integrando 'NewsDetailUpdateDeleteVS' a los 'management/apiv1/routers'

--- apps/management/apiv1/school/routers.py
+++ apps/management/apiv1/school/routers.py
@@ -4,3 +4,5 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register("school", views.SchoolUpdateVS, basename="school")
+router.register("news", views.NewsDetailUpdateDeleteVS, basename="news")
+
