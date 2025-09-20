ad71fabe2558ccb2f5cc01e0f1e0c59af47c5dba
freddyB19 <freddyybolivar2013@gmail.com>
2025-09-19 22:41:30 -0400

    feat: agregando [TimeGroupErrorsMessages, OfficeHourErrorsMessages] a los 'errors_messages' de 'management/commands'

--- apps/management/commands/utils/errors_messages.py
+++ apps/management/commands/utils/errors_messages.py
@@ -1,3 +1,4 @@
+from apps.school import models
 
 class NewsParamErrorsMessages:
 	MAX_LEN = "El título de la noticia es muy largo"
@@ -7,3 +8,17 @@ class NewsParamErrorsMessages:
 
 class SchoolErrorsMessages:
 	DoesNotExist = "No existe información sobre esta escuela"
+
+
+class TimeGroupErrorsMessages:
+	MAX_LEN = models.ERROR_MESSAGE_MAX_LEN_TYPE
+	MIN_LEN = models.ERROR_MESSAGE_MIN_LEN_TYPE
+	INVALID_DAYSWEEK = f"El día de la semana elegido es invalido, debe ser un valor entre: {models.DaysNumber.values}"
+	WRONG_TIME = models.OPENING_CLOSING_TIME
+
+class OfficeHourErrorsMessages:
+	MAX_LEN = models.ERROR_MESSAGE_MIN_LEN_INTERVAL_D
+	MIN_LEN = models.ERROR_MESSAGE_MAX_LEN_INTERVAL_D
+
+	
+	
