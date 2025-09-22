2a63e1206fcf86b987fd769361720e2e55a55443
freddyB19 <freddyybolivar2013@gmail.com>
2025-09-22 17:19:41 -0400

    test-style: agregando funciones a los 'tests/utils' de management
    Entre las funciones agregadas están:
    + set_time
    + set_daysweek
    + selected_daysweek_to_names
    + set_format_dasyweek_query

--- apps/management/tests/utils/utils.py
+++ apps/management/tests/utils/utils.py
@@ -1,9 +1,10 @@
-import tempfile
+import tempfile, datetime
 
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 from apps.management import models
 from apps.management.tests import faker
+from apps.school import models as school_models
 
 from PIL import Image
 
@@ -56,3 +57,40 @@ def list_upload_images() -> list:
 	]
 
 	return images
+
+
+def set_time(hour: int, minute: int) -> str:
+	TIME_FORMAT = '%H:%M:%S'
+
+	return datetime.time(hour = hour, minute = minute).strftime(TIME_FORMAT)
+
+def set_daysweek(days:list[int | str] = [1,2,3,4,5], length:int = 4) -> list[int]:
+	return faker.random_elements(
+		length=length, 
+		unique=True,
+		elements=days
+	)
+
+
+def selected_daysweek_to_names(selected: list[int]) -> list[str]:
+	"""
+		Convierte la lista de enteros [1 - 5] en su correspondiente
+		día de la semana.
+	"""
+	
+	days_dict = {day.value: str(day.name) for day in school_models.DaysNumber}
+	
+	daysweek_names = [days_dict[day] for day in selected]
+
+	return [school_models.DaysName[day].value for day in daysweek_names]
+
+
+def set_format_dasyweek_query(selected: list[int]) -> str:
+	"""
+	Convertimos los días seleccionados en un formato
+	valido para la 'query'
+	[1,4,6] --> '1,4,6'
+	"""
+	selected_daysweek_to_str = [str(day) for day in selected]
+
+	return ",".join(selected_daysweek_to_str)
