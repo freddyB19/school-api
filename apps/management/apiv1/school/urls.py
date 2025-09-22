1386c8ff81b797d4ab31fb0d03903bdd779711bc
freddyB19 <freddyybolivar2013@gmail.com>
2025-09-22 17:54:53 -0400

    feat: integrando 'OfficeHourListCreateAPIView' a las urls de 'management/apiv1/school'

--- apps/management/apiv1/school/urls.py
+++ apps/management/apiv1/school/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
 		"<int:pk>/news/",
 		views.NewsListCreateAPIView.as_view(),
 		name = "news-list-create" 
-	)
+	),
+	path(
+		"<int:pk>/officehour/",
+		views.OfficeHourListCreateAPIView.as_view(),
+		name = "officehour-list-create" 
+	),
 ]
