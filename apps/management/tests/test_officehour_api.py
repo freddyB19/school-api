e0a83ecf7dbb7343831dc7470a64c0afde1a048e
freddyB19 <freddyybolivar2013@gmail.com>
2025-09-22 17:35:53 -0400

    test: agregando tests 'OfficeHourListAPITest' a los 'test_officehour_api' de management

--- apps/management/tests/test_officehour_api.py
+++ apps/management/tests/test_officehour_api.py
@@ -1,16 +1,31 @@
-import random
+import random, datetime
 
 from django.urls import reverse
 
 from . import faker
 
 from apps.school import models
-from apps.school.tests.utils.utils import create_school
+from apps.school.tests.utils.utils import (
+	create_school,
+	create_officehour,
+	create_time_group,
+	bulk_create_officehour,
+	bulk_create_officehour_without_daysweek,
+)
 
 from apps.user.tests.utils.utils import create_user
 
-from .utils.utils import set_time, set_daysweek
-from .utils.testcases import OfficeHourCreateTest
+from .utils.utils import (
+	set_time, 
+	set_daysweek, 
+	set_format_dasyweek_query,
+	selected_daysweek_to_names
+)
+from .utils.testcases import (
+	OfficeHourListTest,
+	OfficeHourCreateTest
+)
+
 
 class OfficeHourCreateAPITest(OfficeHourCreateTest):
 	def setUp(self):
@@ -358,3 +373,267 @@ class OfficeHourCreateAPITest(OfficeHourCreateTest):
 
 		self.assertEqual(responseStatus, 401)
 
+
+class OfficeHourListAPITest(OfficeHourListTest):
+
+	def setUp(self):
+		super().setUp()
+
+		self.officehours_list = bulk_create_officehour(size = 8, school = self.school)
+
+		self.URL_OFFICEHOUR_LIST = self.get_officehour_url(
+			school_id = self.school.id
+		)
+
+	def get_officehour_url(self, school_id, **extra):
+		return reverse(
+			"management:officehour-list-create",
+			kwargs = {"pk": school_id},
+			**extra
+		)
+
+	def test_get_officehour(self):
+		"""
+			Validar "GET /officehour"
+		"""
+		self.client.force_authenticate(user = self.user_with_all_perm)
+
+		response = self.client.get(
+			self.URL_OFFICEHOUR_LIST
+		)
+
+		responseJson = response.data
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 200)
+		self.assertEqual(len(responseJson["results"]), len(self.officehours_list))
+
+	def test_get_officehour_by_filter_active(self):
+		"""
+			Valiar "GET /officehour?is_active=<...>"
+		"""
+		self.client.force_authenticate(user = self.user_with_all_perm)
+
+		yes_active = True
+		no_active = False
+		
+		officehour_with_active_true = models.OfficeHour.objects.filter(
+			time_group__active = yes_active
+		)
+		officehour_with_active_false = models.OfficeHour.objects.filter(
+			time_group__active = no_active
+		)
+
+		response = self.client.get(
+			self.get_officehour_url(
+				school_id = self.school.id,
+				query = {"is_active": yes_active}
+			)
+		)
+
+		responseJson = response.data
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 200)
+		self.assertEqual(
+			len(responseJson["results"]), 
+			officehour_with_active_true.count()
+		)
+		if responseJson["results"]:
+			self.assertTrue(
+				responseJson["results"][0]["time_group"]["active"]
+			)
+
+
+		response = self.client.get(
+			self.get_officehour_url(
+				school_id = self.school.id,
+				query = {"is_active": no_active}
+			)
+		)
+
+		responseJson = response.data
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 200)
+		self.assertEqual(
+			responseJson["count"], 
+			officehour_with_active_false.count()
+		)
+		if responseJson["results"]:
+			self.assertFalse(
+				responseJson["results"][0]["time_group"]["active"]
+			)
+
+
+	def test_get_officehour_by_filter_dasyweek(self):
+		"""
+			Valiar "GET /officehour?days=[<...>,..]			
+		"""
+		self.client.force_authenticate(user = self.user_with_all_perm)
+
+		bulk_create_officehour(size = 10, school = self.school)
+
+		# Indicamos por cuantos días a la semanas queremos hacer la busqueda
+		# Ej: length = 2, [2,3]; length = 1, [4]
+		selected_daysweek = set_daysweek(length = 1)
+
+		# Días de la semana a 'str' 
+		# Ej: [2,4] --> ['Martes', 'Jueves']
+		daysweek_names = selected_daysweek_to_names(
+			selected = selected_daysweek
+		)
+
+		officehour_selected_daysweek = models.OfficeHour.objects.filter(
+			time_group__daysweek__day__in = selected_daysweek,
+			school_id = self.school.id
+
+		).distinct("id")
+
+		response = self.client.get(
+			self.get_officehour_url(
+				school_id = self.school.id,
+				query = {"days": set_format_dasyweek_query(
+					selected = selected_daysweek
+				)}
+			)
+		)
+
+		responseJson = response.data
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 200)
+		self.assertEqual(
+			responseJson["count"],
+			officehour_selected_daysweek.count()
+		)
+
+
+
+	def test_get_officehour_by_filter_dasyweek_null(self):
+		"""
+			Validar "GET /officehour?days_none=true"
+		"""
+		self.client.force_authenticate(user = self.user_with_all_perm)
+
+		total_officehour_without_daysweek = 4
+		isnull = True
+
+		bulk_create_officehour_without_daysweek(
+			school = self.school,
+			size = total_officehour_without_daysweek
+		)
+
+		officehour_without_daysweek = models.OfficeHour.objects.filter(
+			school_id = self.school.id,
+			time_group__daysweek__isnull = isnull
+		)
+
+		response = self.client.get(
+			self.get_officehour_url(
+				school_id = self.school.id,
+				query = {"undays": isnull}
+			)
+		)
+
+		responseJson = response.data
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 200)
+		self.assertEqual(
+			responseJson["count"],
+			officehour_without_daysweek.count()
+		)
+
+	def test_get_officehour_by_filter_description(self):
+		"""
+			Validar "GET /officehour?description__icontains=<...>"
+		"""
+		self.client.force_authenticate(user = self.user_with_all_perm)
+
+		description = faker.text(max_nb_chars = 10)
+
+		officehour = create_officehour(
+			school = self.school,
+			interval_description = description
+		)
+
+		search_description = description[:-3]
+		# description[:-2] = omitimos los '3' útimos caracteres
+
+		response = self.client.get(
+			self.get_officehour_url(
+				school_id = self.school.id,
+				query = {"description": search_description}
+				
+			)
+		)
+
+		officehour_description = models.OfficeHour.objects.filter(
+			school_id = self.school.id,
+			interval_description__icontains = search_description
+		)
+
+		responseJson = response.data
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 200)
+
+		self.assertEqual(
+			responseJson["count"],
+			officehour_description.count()
+		)
+
+
+	def test_get_officehour_without_school_permission(self):
+		"""
+			Generar [Error 403] "GET /officehour" de escuela que no tiene permiso de acceder
+		"""
+		self.client.force_authenticate(user = self.user_with_all_perm)
+
+		other_school = create_school()
+
+		response = self.client.get(
+			self.get_officehour_url(school_id = other_school.id)
+		)
+
+		responseJson = response.data
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 403)
+
+
+	def test_get_officehour_with_wrong_user(self):
+		"""
+			Validar "GET /officehour" por usuario que no forma parte de la administración de la escuela
+		"""
+		user = create_user()
+
+		self.client.force_authenticate(user = user)
+
+		response = self.client.get(
+			self.URL_OFFICEHOUR_LIST
+		)
+
+		responseJson = response.data
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 403)
+
+
+
+
+
+
+	def test_get_officehour_without_authentication(self):
+		"""
+			Validar "GET /officehour" sin autenticación
+		"""
+		response = self.client.get(
+			self.URL_OFFICEHOUR_LIST
+		)
+
+		responseJson = response.data
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 401)
