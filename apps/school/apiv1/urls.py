0d6d70c893277eb90bf01428ee90db506e15872d
freddyB19 <freddyybolivar2013@gmail.com>
2025-10-02 22:51:57 -0400

    style: cambiando nombre de las urls de las api-viwes de school
    Integrando los cambios sobre los nombres de las views relacionadas con
    'ExtraActivity'

--- apps/school/apiv1/urls.py
+++ apps/school/apiv1/urls.py
@@ -46,7 +46,7 @@ urlpatterns = [
 		name='social-media'
 	),
 	path(
-		"<int:pk>/coordinate/", 
+		"<int:pk>/coordinate", 
 		views.CoordinateListAPIView.as_view(), 
 		name='coordinate'
 	),
@@ -56,84 +56,79 @@ urlpatterns = [
 		name='grade'
 	),
 	path(
-		"grade/<int:pk>/", 
+		"grade/<int:pk>", 
 		views.GradeDetailAPIView.as_view(), 
 		name='grade-detail'
 	),
 	path(
-		"<int:pk>/repository/", 
+		"<int:pk>/repository", 
 		views.RepositoryListAPIView.as_view(), 
 		name='repository'
 	),
 	path(
-		"repository/<int:pk>/", 
+		"repository/<int:pk>", 
 		views.RepositoryDetailAPIView.as_view(), 
 		name='repository-detail'
 	),
-	# path(
-	# 	"<int:pk>/service/", 
-	# 	views.SchoolServiceAPIView.as_view(), 
-	# 	name='school-services'
-	# ),
 	path(
-		"<int:pk>/infraestructure/", 
+		"<int:pk>/infraestructure", 
 		views.InfraestructureListAPIView.as_view(), 
 		name='infraestructure'
 	),
 	path(
-		"infraestructure/<int:pk>/", 
+		"infraestructure/<int:pk>", 
 		views.InfraestructureDetailAPIView.as_view(), 
 		name='infraestructure-detail'
 	),
 	path(
-		"<int:pk>/download/", 
+		"<int:pk>/download", 
 		views.DownloadsListAPIView.as_view(), 
 		name='downloads'
 	),
 	path(
-		"download/<int:pk>/", 
+		"download/<int:pk>", 
 		views.DownloadsDetailAPIView.as_view(), 
 		name='downloads-detail'
 	),
 	path(
-		"<int:pk>/news/", 
+		"<int:pk>/news", 
 		views.NewsListAPIView.as_view(), 
 		name='news'
 	),
 	path(
-		"news/<int:pk>/", 
+		"news/<int:pk>", 
 		views.NewsDetailAPIView.as_view(), 
 		name='news-detail'
 	),
 	path(
-		"<int:pk>/cultural/event/", 
+		"<int:pk>/event", 
 		views.CulturalEventsListAPIView.as_view(), 
 		name='cultural-events'
 	),
 	path(
-		"cultural/event/<int:pk>/", 
+		"event/<int:pk>/", 
 		views.CulturalEventsDetailAPIView.as_view(), 
 		name='cultural-events-detail'
 	),
 	path(
-		"<int:pk>/payment/info/", 
+		"<int:pk>/payment", 
 		views.PaymentInfoAPIView.as_view(), 
 		name='payment-info'
 	),
 	path(
-		"<int:pk>/contact/", 
+		"<int:pk>/contact", 
 		views.ContactInfoAPIView.as_view(), 
 		name='contact-info'
 	),
 	path(
-		"<int:pk>/extra/activity/", 
-		views.ExtraActivitiesListAPIView.as_view(), 
-		name='extra-activitie'
+		"<int:pk>/activity", 
+		views.ExtraActivityListAPIView.as_view(), 
+		name='extra-activity'
 	),
 	path(
-		"extra/activity/<int:pk>/", 
-		views.ExtraActivitiesDetailAPIView.as_view(), 
-		name='extra-activitie-detail'
+		"activity/<int:pk>/", 
+		views.ExtraActivityDetailAPIView.as_view(), 
+		name='extra-activity-detail'
 	),
 ]
 
