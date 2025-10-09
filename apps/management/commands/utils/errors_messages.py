a284a29187939936394fe08eaae4df01326c98de
freddyB19 <freddyybolivar2013@gmail.com>
2025-10-08 22:33:43 -0400

    feat: definiendo mensaje de error (DOES_NOT_EXIST) para 'TimeGroupErrorsMessages'
    Este cambio forma parte de los errors_messages de management/commands

--- apps/management/commands/utils/errors_messages.py
+++ apps/management/commands/utils/errors_messages.py
@@ -12,6 +12,7 @@ class SchoolErrorsMessages:
 
 class TimeGroupErrorsMessages:
 	MAX_LEN = models.ERROR_MESSAGE_MAX_LEN_TYPE
+	DOES_NOT_EXIST = "No existe información sobre este grupo horario"
 	MIN_LEN = models.ERROR_MESSAGE_MIN_LEN_TYPE
 	INVALID_DAYSWEEK = f"El día de la semana elegido es invalido, debe ser un valor entre: {models.DaysNumber.values}"
 	WRONG_TIME = models.OPENING_CLOSING_TIME
