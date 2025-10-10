4e96d1cf2c4f6afc5285b86a5741e040a6115200
freddyB19 <freddyybolivar2013@gmail.com>
2025-10-10 00:02:14 -0400

    test: agregando tests 'NewsMediaDetailDeleteAPITest' en management

---
+++ tests/management/test_newsmedia_api.py
@@ -0,0 +1,132 @@
+from django.urls import reverse
+
+from .utils import testcases
+
+from apps.school import models
+
+from tests import faker
+from tests.school.utils import create_news_media
+
+
+class NewsMediaDetailDeleteAPITest(testcases.NewsMediaTestCase):
+	def setUp(self):
+		super().setUp()
+		self.newsmedia = create_news_media()
+
+		self.URL_DETAIL_NEWSMEDIA = self.get_detail_newsmedia_url(
+			id = self.newsmedia.id
+		)
+
+	def get_detail_newsmedia_url(self, id):
+		return reverse(
+			"management:news-images-detail",
+			kwargs = {"pk": id}
+		)
+
+	def test_detail_newsmedia(self):
+		"""
+			Validar "GET /news/images/:id" 
+		"""
+		self.client.force_authenticate(user = self.user_with_delete_perm)
+
+		response = self.client.get(
+			self.URL_DETAIL_NEWSMEDIA
+		)
+
+		responseJson = response.data
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 200)
+		self.assertEqual(responseJson["id"], self.newsmedia.id)
+
+
+	def test_detail_newsmedia_does_not_exist(self):
+		"""
+			Generar [Error 404] "GET /news/images/:id" por ID que no existe 
+		"""
+		self.client.force_authenticate(user = self.user_with_delete_perm)
+
+		news_media_id = models.NewsMedia.objects.last().id
+		
+		wrong_id = faker.random_int(min = news_media_id + 1)
+		
+
+		response = self.client.get(
+			self.get_detail_newsmedia_url(id = wrong_id)
+		)
+
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 404)
+
+	def test_detail_newsmedia_without_authentication(self):
+		"""
+			Generar [Error 401] "GET /news/images/:id" sin autenticación
+		"""
+		response = self.client.get(
+			self.URL_DETAIL_NEWSMEDIA
+		)
+
+		responseJson = response.data
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 401)
+
+	def test_delete_newsmedia(self):
+		"""
+			Validar "DELETE /news/images/:id"
+		"""
+		self.client.force_authenticate(user = self.user_with_delete_perm)
+
+		response = self.client.delete(
+			self.URL_DETAIL_NEWSMEDIA
+		)
+
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 204)
+
+	def test_delete_newsmedia_does_not_exist(self):
+		"""
+			Generar [Error 404] "DELETE /news/images/:id" por ID que no existe 
+		"""
+		self.client.force_authenticate(user = self.user_with_delete_perm)
+
+		news_media_id = models.NewsMedia.objects.last().id
+		
+		wrong_id = faker.random_int(min = news_media_id + 1)
+
+		response = self.client.delete(
+			self.get_detail_newsmedia_url(id = wrong_id)
+		)
+
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 404)
+
+	def test_delete_newsmedia_without_user_permissions(self):
+		"""
+			Generar [Error 403] "DELETE /news/images/:id" por usuario sin permisos
+		"""
+
+		self.client.force_authenticate(user = self.user_with_add_perm)
+
+		response = self.client.delete(
+			self.URL_DETAIL_NEWSMEDIA
+		)
+
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 403)
+
+	def test_delete_newsmedia_without_authentication(self):
+		"""
+			Generar [Error 401] "DELETE /news/images/:id" sin autenticación
+		"""
+		response = self.client.delete(
+			self.URL_DETAIL_NEWSMEDIA
+		)
+
+		responseStatus = response.status_code
+
+		self.assertEqual(responseStatus, 401)
