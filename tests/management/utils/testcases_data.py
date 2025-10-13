10af9ff54fc260f38ee6b4b95517d858c584456a
freddyB19 <freddyybolivar2013@gmail.com>
2025-10-12 21:56:15 -0400

    test-feat: agregando datos para tests de 'update-timegroup' en 'testcases_data'
    Estos cambios forman parte de los tests de management

--- tests/management/utils/testcases_data.py
+++ tests/management/utils/testcases_data.py
@@ -1,3 +1,4 @@
+import datetime
 from apps.school import models as school_models
 
 from tests import faker
@@ -81,5 +82,33 @@ UPDATE_OFFICEHOUR_WITH_WRONG_DATA = [
 		"description": faker.pystr(
 			max_chars = school_models.MIN_LENGTH_OFFICEHOUR_INTERVAL_D - 1,
 		)
+	}
+]
+
+
+UPDATE_TIMEGROUP_WITH_WRONG_DATA = [
+
+	{
+		"update": {
+			"type": faker.pystr(
+				max_chars = school_models.MAX_LENGTH_TYPEGROUP_TYPE + 1
+			)
+		}
+	},
+	{
+		"update": {
+			"daysweek": set_daysweek(days = [6,7,2,9,8,10,1], length = 3) 
+		}
+	},
+	{
+		"update": {
+			"opening_time": datetime.time(15, 30),
+			"closing_time": datetime.time(12, 10)
+		}
+	},
+	{
+		"update": {
+			"opening_time": datetime.time(15, 30),
+		}
 	}
 ]
