46d63b635ff76ea38f0415516172bbff390da73a
freddyB19 <freddyybolivar2013@gmail.com>
2025-10-15 23:28:20 -0400

    feat: agregando paginación personalizada para vistas sobre 'Calendar'
    Este cambio forma parte de la pagianción de management/apiv1/school

--- apps/management/apiv1/school/paginations.py
+++ apps/management/apiv1/school/paginations.py
@@ -6,3 +6,10 @@ class BasicPaginate(pagination.PageNumberPagination):
 	max_page_size = 20
 	page_query_param = "page"
 	page_size_query_param = "size"
+
+
+class CalendarPaginate(pagination.PageNumberPagination):
+	page_size = 30
+	max_page_size = 50
+	page_query_param = "page"
+	page_size_query_param = "size"
