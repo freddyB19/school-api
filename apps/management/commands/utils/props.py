aa49eaba69c33df3659f2b2a12b2a8ded9b1c541
freddyB19 <freddyybolivar2013@gmail.com>
2025-10-17 17:57:52 -0400

    feat: eliminando validaciones innecesarias relacionadas con 'News' de los 'commands/utils/props'
    Esto se debe a que todas las implementaciones de las funciones
    'commands' sobre 'News' ya están siendo validadas mediante los serializadores, por lo
    tanto, no es necesario repetir estas validaciones.
    
    Este cambio forma parte de los 'commands' de management

--- apps/management/commands/utils/props.py
+++ apps/management/commands/utils/props.py
@@ -1,26 +1,12 @@
 import datetime
 from typing import TypeVar
-from typing_extensions import Annotated
 
 from django.utils.datastructures import MultiValueDict
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
-from pydantic import (
-	BaseModel, 
-	AfterValidator,
-	PlainValidator,
-	field_validator, 
-	model_validator,
-)
+from pydantic import BaseModel
 
 from apps.school import models
-from apps.utils.functions import validate_choice
-
-from .errors_messages import (
-	NewsParamErrorsMessages,
-	TimeGroupErrorsMessages,
-	OfficeHourErrorsMessages
-)
 
 
 UploadedFile = TypeVar("UploadedFile", bound=InMemoryUploadedFile)
@@ -29,30 +15,10 @@ DjangoDict = TypeVar("DjangoDict", bound=MultiValueDict)
 TimeGroupModel = TypeVar("TimeGroupModel", bound=models.TimeGroup)
 
 
-VALIDATE_MAX_LEN = lambda value, max_len: value > max_len
-VALIDATE_MIN_LEN = lambda value, min_len: value < min_len
-
-
 class NewsParam(BaseModel):
 	title: str 
 	description: str | None = None
 	status: str | None = None
-
-	@field_validator('title', mode='after')
-	@classmethod
-	def length_title(cls, value):
-		if VALIDATE_MAX_LEN(value = len(value), max_len = models.MAX_LENGTH_NEWS_TITLE):
-			raise ValueError(NewsParamErrorsMessages.MAX_LEN)
-		elif VALIDATE_MIN_LEN(value = len(value), min_len = models.MIN_LENGTH_NEWS_TITLE):
-			raise ValueError(NewsParamErrorsMessages.MIN_LEN)
-		return value
-
-	@field_validator("status", mode= "after")
-	@classmethod
-	def choice_status(cls, value):
-		if not validate_choice(choice = value, options = models.News.TypeStatus):
-			raise ValueError(NewsParamErrorsMessages.INVALID_STATUS)
-		return value
 
 class TimeGroupByIdParam(BaseModel):
 	id: int
