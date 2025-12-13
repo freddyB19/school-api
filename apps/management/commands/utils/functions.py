16edefd71fcbfb7deeb60bd66857bc77da73c028
freddyB19 <freddyybolivar2013@gmail.com>
2025-12-12 20:43:30 -0400

    style: permitiendo el uso de la función con diferentes archivos en las 'utils/commands' de managemet

--- apps/management/commands/utils/functions.py
+++ apps/management/commands/utils/functions.py
@@ -2,18 +2,18 @@ import uuid
 
 from pydantic import validate_call, ConfigDict
 
-IMAGE_FORMAT_LEN = 2
-UNKNOWN_IMAGE_FORMAT = None
+FILE_FORMAT_LEN = 2
+UNKNOWN_FILE_FORMAT = None
 
 
 @validate_call(config = ConfigDict(hide_input_in_errors=True))
-def set_name_image(image_name: str) -> str | None:
-	format_image = image_name.split(".")
+def set_name_file(file_name: str) -> str | None:
+	format_file = file_name.split(".")
 
-	if len(format_image) == IMAGE_FORMAT_LEN:
+	if len(format_file) == FILE_FORMAT_LEN:
 
-		ext = format_image[-1]
+		ext = format_file[-1]
 
 		return f"{uuid.uuid4()}.{ext}"
 
-	return UNKNOWN_IMAGE_FORMAT
+	return UNKNOWN_FILE_FORMAT
