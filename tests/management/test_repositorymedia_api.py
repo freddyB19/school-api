f9af59137cf2e98c9aa9a396b7105cd834e34478
freddyB19 <freddyybolivar2013@gmail.com>
2026-01-09 21:39:05 -0400

    test: agregando tests sobre [delete] de 'api-repository-media' en management

--- tests/management/test_repositorymedia_api.py
+++ tests/management/test_repositorymedia_api.py
@@ -66,3 +66,66 @@ class RepositoryMediaFileDetailAPITest(testcases.RepositoryMediaFileTestCase):
 		responseStatusCode = response.status_code
 
 		self.assertEqual(responseStatusCode, 401)
+
+
+class RepositoryMediaFileDeleteAPITest(testcases.RepositoryMediaFileTestCase):
+	def setUp(self):
+		super().setUp()
+
+		self.repository_media = create_respository_media_file()
+		self.URL_REPOSITORY_MEDIA_DELETE = get_detail_repository_media(
+			id = self.repository_media.id
+		)
+
+	def test_delete_repository_media(self):
+		"""
+			Validar "DELETE /repository/file/:id"
+		"""
+		self.client.force_authenticate(user = self.user_with_delete_perm)
+
+		response = self.client.delete(self.URL_REPOSITORY_MEDIA_DELETE)
+
+		responseStatusCode = response.status_code
+
+		self.assertEqual(responseStatusCode, 204)
+
+	def test_delete_repository_media_without_user_permission(self):
+		"""
+			Generar [Error 403] "DELETE /repository/file/:id" por usuario sin permisos
+		"""
+		self.client.force_authenticate(user = self.user_with_view_perm)
+
+		response = self.client.delete(self.URL_REPOSITORY_MEDIA_DELETE)
+
+		responseJson = response.data
+		responseStatusCode = response.status_code
+
+		self.assertEqual(responseStatusCode, 403)
+
+	def test_delete_repository_media_does_not_exit(self):
+		"""
+			Generar [Error 404] "DELETE /repository/file/:id" por ID que no existe
+		"""
+		self.client.force_authenticate(user = self.user_with_delete_perm)
+
+		repository_media_id = models.RepositoryMediaFile.objects.last().id
+
+		wrong_id = faker.random_int(min = repository_media_id + 1)
+
+		response = self.client.delete(
+			get_detail_repository_media(id = wrong_id)
+		)
+
+		responseStatusCode = response.status_code
+
+		self.assertEqual(responseStatusCode, 404)
+
+	def test_delete_repository_media_without_authentication(self):
+		"""
+			Generar [Error 401] "DELETE /repository/file/:id" sin autenticación
+		"""
+		response = self.client.delete(self.URL_REPOSITORY_MEDIA_DELETE)
+
+		responseStatusCode = response.status_code
+
+		self.assertEqual(responseStatusCode, 401)
